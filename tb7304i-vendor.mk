f05fbe660070e2c75f1eac8231880cc467a06e54
shifty-d <ffoxy2870@gmail.com>
2023-09-08 15:22:34 +0300

    first commit

---
+++ tb7304i-vendor.mk
@@ -0,0 +1,17 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/tb7304i/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/tb7304i/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/tb7304i/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/tb7304i/proprietary/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/tb7304i/proprietary/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/tb7304i/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/tb7304i/proprietary/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/tb7304i/proprietary/vendor/lib,system/vendor/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/tb7304i/proprietary/vendor/lib64,system/vendor/lib64) 
